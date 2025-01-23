bc5927422cc6a12d1cd73e232d133f30c05a40b0
Arsen <arsenanai@gmail.com>
2025-01-23 18:57:40 +0500

    adding a language file

---
+++ lang/en/local_quickstats.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Language file for the QuickStats plugin.
+ *
+ * @package    local_quickstats
+ * @copyright  2025 Arsen Anay
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['pluginname'] = 'QuickStats';
+$string['days'] = 'Number of days';
+$string['days_desc'] = 'Number of days to display data for';
+$string['enableplugin'] = 'Enable QuickStats plugin';
+$string['enableplugin_desc'] = 'Enable or disable the QuickStats plugin';
+// $string['viewplugin'] = 'view the QuickStats plugin';
+// $string['nopermissions'] = 'Sorry, but you do not currently have permissions to {$a}.';
+// $string['disabledplugin'] = 'Sorry, but the QuickStats plugin is currently disabled.';
+?>
