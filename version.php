828bb8f3e03068a5c231f8ee9da57d331939855b
Arsen <arsenanai@gmail.com>
2025-01-23 18:46:07 +0500

    Adding a version file

---
+++ version.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Version information for the QuickStats plugin.
+ *
+ * @package    local_quickstats
+ * @copyright  2025 Arsen Anay
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_quickstats';
+$plugin->version = 2025012300;
+$plugin->requires = 2020110900; // Moodle 3.10+.
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0';
+?>
